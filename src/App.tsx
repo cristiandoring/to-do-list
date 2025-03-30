3d6d440377049a41fb033859eb0ba0e7f6ef3e34
Cristian Doring <cristiandoring@gmail.com>
2025-03-30 19:36:04 -0300

    feat: criei a estrutura do projeto e a parte estatica do input

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import NewTask from "./components/NewTask"
+function App() {
+   return (
+    <section className="bg-gradient-to-t from-cyan-800 to-cyan-500 h-screen w-full flex flex-col items-center ">
+      <div className="min-w-150 border border-neutral-400 rounded-md  mt-56 bg-neutral-100" >
+      <h1 className="text-black font-bold text-3xl w-full text-center p-6">To do List</h1>
+      <NewTask/>
+      </div>
+    </section>
+
+  )
+}
+
+export default App
---
+++ src/components/NewTask.tsx
@@ -0,0 +1,12 @@
+const NewTask = () => {
+  return (
+    <form className="w-full flex items-center gap-4 justify-center mb-4">
+      <label>
+        <input type="text" placeholder="Add your new task" className="border-2 border-neutral-400 rounded-md w-110 px-4 py-2 text-black h-12 outline-neutral-500 text-[20px] hover:cursor-pointer"/>
+      </label>
+      <button className="h-12 w-12 flex justify-center items-center font-bold text-white text-3xl bg-purple-500 rounded-md hover:cursor-pointer">+</button>
+    </form>
+  );
+};
+
+export default NewTask;
